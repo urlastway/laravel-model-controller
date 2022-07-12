4228a2aa69589ce65a84fc0e1c763d93bcab10a2
urlastway <andriy.semenchenko13@gmail.com>
2022-07-12 22:14:16 +0200

    first commit

---
+++ app/Http/Controllers/MovieController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\movie;
+
+class MovieController extends Controller
+
+{
+    public function index(){
+        $movie = Movie::all();
+        dd($movie);
+    }
+}
